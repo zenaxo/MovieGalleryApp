feab631ee2c69efa40acbd5e0f984ef7431497eb
oskar-wahlberg <32671858+oskar-wahlberg@users.noreply.github.com>
2023-11-23 15:52:44 +0100

    gfhmny ghmfj

---
+++ MovieGallery/SQL/GetAverageRating.sql
@@ -0,0 +1,15 @@
+﻿SELECT
+    MovieID,
+    AVG(CAST(Rating AS FLOAT)) AS AverageRating
+FROM
+    Ratings
+GROUP BY
+    MovieID;
+
+SELECT
+    MovieID,
+    COUNT(*) AS NumberOfRatings
+FROM
+    Ratings
+GROUP BY
+    MovieID;
