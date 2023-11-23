1f5bd624cd0ecef3be117cfe3d503b39989046a6
zenaxo <sjolander48@live.se>
2023-11-23 16:39:06 +0100

    tjho

---
+++ MovieGallery/Views/Movies/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MovieGallery.Views.Movies
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
